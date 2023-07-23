39575c2a8c9dd97bf33006af910dc8fa1ef9ca7d
tony007x <aekanan2004@gmail.com>
2023-07-23 14:48:16 +0700

    Hello world **5

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,16 @@ function App() {
   return (
 
     <div className='App'>
-      <h1>Hello World!</h1>
+      <h2>Hello World!</h2>
+      <h2>Hello World!</h2>
+      <h2>Hello World!</h2>
+      <h2>Hello World!</h2>
+      <h2>Hello World!</h2>
+      <h2>Hello World!</h2>
+      <h2>Hello World!</h2>
+      <h2>Hello World!</h2>
+
+
       <About data={data} func={setState} />
       {
         data.map((item, idx) => {
